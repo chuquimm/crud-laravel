5b6874196841821ecb0caede293159a4bac1c04b
Marcos Chuquicondor <marcoschuquicondor@gmail.com>
2018-12-27 00:08:43 -0500

    edit partials

--- resources/views/admin/template/partials/footer.blade.php
+++ resources/views/admin/template/partials/footer.blade.php
@@ -2,16 +2,15 @@
         <div class="container">
           <div class="row">
             <div class="col l6 s12">
-              <h5 class="white-text">Footer Content</h5>
-              <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              <h5 class="white-text">Footer</h5>
+              <p class="grey-text text-lighten-4">This CRUD is an example.</p>
             </div>
             <div class="col l4 offset-l2 s12">
               <h5 class="white-text">Links</h5>
               <ul>
-                <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                <li><a class="grey-text text-lighten-3" href="https://github.com/chuquimm">Github</a></li>
+                <li><a class="grey-text text-lighten-3" href="https://www.instagram.com/chuquimm/">Instagram</a></li>
+                <li><a class="grey-text text-lighten-3" href="https://twitter.com/chuquimm">Twitter</a></li>
               </ul>
             </div>
           </div>
@@ -19,7 +18,6 @@
         <div class="footer-copyright">
           <div class="container">
           © 2014 Copyright Text
-          <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
           </div>
         </div>
       </footer>
--- resources/views/admin/template/partials/nav.blade.php
+++ resources/views/admin/template/partials/nav.blade.php
@@ -1,10 +1,8 @@
 <nav>
     <div class="nav-wrapper">
-        <a href="#" class="brand-logo">Logo</a>
+        <a href="#" class="brand-logo">CRUD</a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
+        <li><a href="">Agregar</a></li>
         </ul>
     </div>
 </nav>
