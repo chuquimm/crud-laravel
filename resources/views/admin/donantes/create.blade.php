9d1843d43edf212eb02d865858ecbe0e6f340754
Marcos Chuquicondor <marcoschuquicondor@gmail.com>
2018-12-27 15:23:01 -0500

    add ubicacion

--- resources/views/admin/donantes/create.blade.php
+++ resources/views/admin/donantes/create.blade.php
@@ -12,6 +12,7 @@
                     {!! Form::text('nombre', null, ['id' => 'nombre', 'type' => 'text', 'class' => 'validate', 'requiered']) !!}
                     {!! Form::label('nombre', 'Nombre') !!}
                 </div>
+
                 <div class="input-field col s6">
                     {!! Form::text('apellido', null, ['id' => 'apellido', 'type' => 'text', 'class' => 'validate', 'requiered'] ) !!}
                     {!! Form::label('apellido', 'Apellido') !!}
@@ -37,6 +38,7 @@
                         {!! Form::text('dni', null, ['id' => 'dni', 'type' => 'text', 'class' => 'validate', 'requiered']) !!}
                         {!! Form::label('dni', 'DNI') !!}
                     </div>
+
                     <div class="input-field col s6">
                         {!! Form::text('celular', null, ['id' => 'celular', 'type' => 'text', 'class' => 'validate', 'requiered'] ) !!}
                         {!! Form::label('celular', 'Celular') !!}
@@ -62,10 +64,12 @@
                     {!! Form::select('sangre', ['A' => 'A', 'B' => 'B', 'AB' => 'AB', 'O' => 'O']) !!}
                     {!! Form::label('sangre', 'Sangre') !!}
                 </div>
+
                 <div class="input-field col s4">
                     {!! Form::select('factor', ['+' => '-', '-' => '-']) !!}
                     {!! Form::label('factor', 'Factor') !!}
                 </div>
+
                 <div class="col s4">
                     <label>
                         {{-- <input type="checkbox"> --}}
@@ -73,6 +77,13 @@
                         <span>No conozco mi tipo de sangre</span>
                         {{-- {!! Form::label("desconoce", "Label") !!} --}}
                     </label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    {!! Form::text('ubicacion', null, ['id' => 'ubicacion', 'type' => 'text', 'class' => 'validate', 'requiered']) !!}
+                    {!! Form::label('ubicacion', 'Ubicacion') !!}
                 </div>
             </div>
 
